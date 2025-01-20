ecbf40be3c5be620904acb74b26e2bda475c36a6
flengyel <florian.lengyel@gmail.com>
2025-01-20 15:19:08 -0500

    test_display_init replaced with test_display_init_new

--- tests/ui/test_display_init.py
+++ tests/ui/test_display_init.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
-
 import curses
 import time
 from src.ui.constants import ColorPair
-from src.ui.windows.status_window import StatusWindow
-from src.ui.windows.receive_window import ReceiveWindow
-from src.ui.windows.transmit_window import TransmitWindow
+from src.ui.display_init import initialize_display
 
 def test_basic_window(stdscr):
     # Initialize color pairs
     curses.start_color()
     curses.use_default_colors()
-
     curses.init_color(curses.COLOR_RED, 1000, 0, 0)
     curses.init_color(curses.COLOR_GREEN, 0, 1000, 0)
     curses.init_color(curses.COLOR_BLUE, 0, 0, 1000)
-
     curses.init_pair(ColorPair.YELLOW_BLACK.value, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(ColorPair.GREEN_BLACK.value, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(ColorPair.BLUE_BLACK.value, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -26,22 +21,9 @@ def test_basic_window(stdscr):
     curses.init_pair(ColorPair.WHITE_GREEN.value, curses.COLOR_WHITE, curses.COLOR_GREEN)
 
     def run_test():
-        # Create main border window
-        border_win = curses.newwin(28, 42, 0, 0)
-        border_win.border()
-        
-        # Draw horizontal lines for status area
-        for y in [21, 23]:
-            border_win.addch(y, 0, curses.ACS_LTEE)
-            border_win.hline(y, 1, curses.ACS_HLINE, 40)
-            border_win.addch(y, 41, curses.ACS_RTEE)
-        
-        border_win.refresh()
-
-        # Create all windows using their classes
-        receive = ReceiveWindow(border_win)
-        status = StatusWindow(border_win)
-        transmit = TransmitWindow(border_win)
+        # Get windows from initialize_display
+        # The order is border_win, status, receive, transmit
+        _, status, receive, transmit = initialize_display(stdscr)
 
         # Test each window
         receive.add_line("Testing receive window...")
